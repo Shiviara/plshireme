cc7fd19c223d58316834b2a7f5a513b66512836d
Shiviara <113165919+Shiviara@users.noreply.github.com>
2023-09-27 23:41:10 +0200

    copy with feedback

--- js/copy.js
+++ js/copy.js
@@ -6,6 +6,10 @@ const copyContent = async () => {
   try {
     await navigator.clipboard.writeText(text);
     console.log("Content copied to clipboard");
+    document.getElementById("feedback").style.opacity = "1";
+    setTimeout(() => {
+      document.getElementById("feedback").style.opacity = "0";
+    }, 3000);
   } catch (err) {
     console.error("Failed to copy: ", err);
   }
