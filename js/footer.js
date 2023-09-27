36a7015058c045607a43003204a7c7abac65c298
Shiviara <113165919+Shiviara@users.noreply.github.com>
2023-09-27 19:16:36 +0200

    footer in js

---
+++ js/footer.js
@@ -0,0 +1,37 @@
+console.log("Adding navigation and footer");
+
+//Universal footer with copyright
+const Footer = `
+    <!--Contact-->
+        <div class="left">
+          <div id="contact">
+            <h3>Marta Zarebska</h3>
+              <div class="iconText">
+                  <a href="https://www.linkedin.com/in/marta-zarebska/" target="_blank"><img src="/img/linkedin.svg" alt="LinkedIn"></a>
+                  <span class="material-symbols-outlined"> mail</span>
+              <p>martazarebska@plshireme.com</p>
+              </div>
+          </div>
+        <p id="madeby">Designed & developed by me</p>
+        </div>
+
+        <div class="right">
+
+            <!--Navigation-->
+            <div class="column">
+            <h5>Navigation</h5>
+            <a href="#hero">Home</a>
+            <a href="#work">Work</a>
+            <a href="aboutme.html">About me</a>
+            </div>
+
+            <div class="column">
+            <h5>Work</h5>
+            <a href="tcc.html">The Clothing Club</a>
+          <a href="smukfest.html">Smukfest</a>
+          <a href="ecobites.html">EcoBites</a>
+            </div>
+        </div>
+`;
+
+document.getElementById("finalFooter").innerHTML = Footer;
