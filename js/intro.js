133b29e351b333827b822a613a7f53777c70d77b
Shiviara <113165919+Shiviara@users.noreply.github.com>
2023-09-28 21:40:07 +0200

    animation first try

---
+++ js/intro.js
@@ -0,0 +1,18 @@
+console.log("Looking for a cat");
+
+/*var id = null;
+function myMove() {
+  var elem = document.getElementById("grumpy");
+  var pos = 0;
+  clearInterval(id);
+  id = setInterval(frame, 10);
+  function frame() {
+    if (pos == 350) {
+      clearInterval(id);
+    } else {
+      pos++;
+      elem.style.top = pos + "px";
+      elem.style.left = pos + "px";
+    }
+  }
+}*/
