9c1bed19bcd8f4636e486daac7b83e82f6b9c7cf
Shiviara <113165919+Shiviara@users.noreply.github.com>
2023-09-28 22:31:30 +0200

    basic change text

--- js/intro.js
+++ js/intro.js
@@ -3,3 +3,24 @@ console.log("Looking for a cat");
 setTimeout(function () {
   document.getElementById("speechBubble").textContent = "...";
 }, 5000);
+
+/*
+//Text changes gradually
+function changeText() {
+  const textToChange = document.getElementById("speechBubble");
+  const originalText = textToChange.innerText;
+  textToChange.innerText = "...";
+
+  let currentIndex = originalText.length;
+  const interval = setInterval(function () {
+    currentIndex--;
+    textToChange.innerText = originalText.slice(0, currentIndex) + "...";
+
+    if (currentIndex === 0) {
+      clearInterval(interval);
+    }
+  }, 100); // Adjust the interval for the speed of the animation
+}
+
+setTimeout(changeText, 2000);
+*/
