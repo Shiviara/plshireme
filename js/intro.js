723dee297106584c596f804d0f9fd85cfe4085c8
Shiviara <113165919+Shiviara@users.noreply.github.com>
2023-09-28 23:03:25 +0200

    cat looks at you

--- js/intro.js
+++ js/intro.js
@@ -8,14 +8,15 @@ setTimeout(function () {
   document.getElementById("speechBubble").textContent = "It's so boring.";
 
   setTimeout(function () {
-    document.getElementById("speechBubble").textContent = "...";
+    document.getElementById("speechBubble").textContent = "I mean really?";
 
     setTimeout(function () {
       document.getElementById("speechBubble").textContent = "Hey, you!";
+      document.getElementById("grumpy").src = "/img/cat/looking.svg";
 
       setTimeout(function () {
         document.getElementById("speechBubble").textContent =
-          "Click this button!";
+          "Click that button!";
       }, 1000);
     }, 3000);
   }, 2000);
