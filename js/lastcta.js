044b586267b58efd0058ba4787ea9fde5821d534
Shiviara <113165919+Shiviara@users.noreply.github.com>
2023-10-04 12:18:33 +0200

    feeedback

--- js/lastcta.js
+++ js/lastcta.js
@@ -2,10 +2,10 @@
 const LastCTA = `
     <img src="/img/greenWaveTop.svg" class="waves">
         <h3>Do you have an idea brewing? </h3>
-        <p>Let's talk it over some tea</p>
+        <p>Let's talk about it over some tea</p>
         <p id="feedback">📬 My email is now copied to your clipboard ;)</p>
         <button class="secondary-cta" onclick="copyContent()">
-          Contact me
+          Email me
         </button>
         <img src="/img/greenWaveBottom.svg" class="waves">
 `;
